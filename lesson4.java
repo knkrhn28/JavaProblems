77fae061650b545a00fd5c3f9f4d2f3b91d0c0c9
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-13 13:21:20 +0300

    New Started

---
+++ lesson4.java
@@ -0,0 +1,27 @@
+package JavaLearn;
+
+public class lesson4 {
+
+	public static void main(String[] args) {
+		int a=45 ,b=2 , c=45 ,d=2;
+		a++;
+		++b;
+		c--;
+		--d;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+		System.out.println("d = "+d);
+
+		
+		
+		int sayi1=7;
+		int sayi2=7;
+		int sayi3=sayi1++;
+		int sayi4=++sayi2;
+		
+		System.out.println("Sayi 3 = "+sayi3);
+		System.out.println("Sayi 4 = "+sayi4);
+	}
+
+}
