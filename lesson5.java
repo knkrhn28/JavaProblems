e1ab90d367ef9cce9cfe395b1ddb3d511d3d1dba
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-13 13:29:46 +0300

    Fibonnaci Series Using Recursion

---
+++ lesson5.java
@@ -0,0 +1,17 @@
+package JavaLearn;
+
+public class lesson5 {
+	static int fib (int n)
+	{
+		if (n <= 1)
+			return n;
+		return fib(n-1) + fib(n-2);
+		
+	}
+
+	public static void main(String[] args) {
+		int n=9;
+		System.out.println(n+".inci fibonacci sayýsý:"+fib(n));
+	}
+
+}
