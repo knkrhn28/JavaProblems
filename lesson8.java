fdaf92056feb019af5a75c17e271c454fe6b0992
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-19 13:18:41 +0300

    ASCII

---
+++ lesson8.java
@@ -0,0 +1,12 @@
+package JavaLearn;
+
+public class lesson8 {
+
+	public static void main(String[] args) {
+		for (char c=0;c<256;c++) {
+			System.out.println("Deðer: "+ (int)(c)+ " Karakter: "+ c);
+		}
+
+	}
+
+}
