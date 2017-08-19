14239ea50ef850284d92b455d70e247094ab322b
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-19 13:20:18 +0300

    ASCII

--- lesson8.java
+++ lesson8.java
@@ -4,7 +4,7 @@ public class lesson8 {
 
 	public static void main(String[] args) {
 		for (char c=0;c<256;c++) {
-			System.out.println("Deðer: "+ (int)(c)+ " Karakter: "+ c);
+			System.out.println("Deger: "+ (int)(c)+ " Karakter: "+ c);
 		}
 
 	}
