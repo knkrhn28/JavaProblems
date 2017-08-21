2348e063976ce8ab6e6259e82d9ba5453617016c
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-21 17:32:21 +0300

    ArrayPrint

---
+++ Lesson0005.java
@@ -0,0 +1,9 @@
+package JavaEducation;
+public class Lesson0005 {
+	public static void main(String[] args) {
+		double[] liste= {1.9, 2.9, 3.4, 3.5};
+		for (int i=0;i<liste.length;i++) {
+			System.out.println(liste[i]+" ");			
+		}
+	}
+}
