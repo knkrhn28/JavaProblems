49236f377a4dec273c9a24457feb35db2a40369e
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-22 19:22:49 +0300

    app

---
+++ Lesson0007.java
@@ -0,0 +1,15 @@
+package JavaEducation;
+import java.io.*;
+public class Lesson0007 {
+
+	public static void main(String[] args)throws IOException {
+		String kelime = "money";int i,tahmin=0,anahtar=0,dogru=0;
+        String eldevar[] = new String[kelime.length()];
+
+        System.out.println("Kelimeyi bulmak için 4 yanlýþ hakkýnýz var.");
+        BufferedReader klavye = new BufferedReader(new InputStreamReader(System.in));
+
+        
+	}
+
+}
