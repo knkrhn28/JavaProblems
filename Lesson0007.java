e4ac05cb227f7f37119d2e3a5b1cc9040bcdc5cc
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-22 19:23:54 +0300

    app

--- Lesson0007.java
+++ Lesson0007.java
@@ -9,7 +9,36 @@ public class Lesson0007 {
         System.out.println("Kelimeyi bulmak için 4 yanlýþ hakkýnýz var.");
         BufferedReader klavye = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        for (i = 0; i < kelime.length(); i++)
+        {
+                eldevar[i] = "_ ";
+
+        }
+
+        finish:
+        while(tahmin<6){
+                System.out.println("Bir harf giriniz (Kalan hakkiniz "+(4 -tahmin) +") : ");
+                String harf = klavye.readLine();
+                tahmin++;
+                for (i = 0; i < kelime.length(); i++)
+                {
+                        if (kelime.charAt(i) == harf.charAt(0))
+                        {
+                                eldevar[i] = harf+" ";
+                                anahtar = 1;
+                                dogru++;
+                                if (dogru == kelime.length()) { System.out.println("kelime " +kelime+"...Tebrikler..."); break finish; }
+                        }
+                }
+                if (anahtar == 1) { anahtar = 0; tahmin--; }
+                for (i = 0; i < kelime.length(); i++)
+                {
+                        System.out.print(eldevar[i]);
+
+                }
+                System.out.println();
+        }
+        if (dogru != kelime.length()) { System.out.println("Uzgunum... Dogru yanit " + kelime); }
 	}
 
 }
