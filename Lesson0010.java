53c4f6e760e59be4065c16a2926646a9e25562d8
Okan KARAHAN <oqan_28@hotmail.com>
2017-08-23 18:56:09 +0300

    bubbleSort
    kabarcikSiralama

--- Lesson0010.java
+++ Lesson0010.java
@@ -15,13 +15,13 @@ public class Lesson0010 {
 	}
 	public static void main(String[] args) {
 		 int dizi[] ={3,60,35,2,45,320,5};  
-         System.out.println("Sýrasýz Dizi:");  
+         System.out.println("Sirasiz Dizi:");  
          for(int i=0; i < dizi.length; i++){  
                  System.out.print(dizi[i] + " ");  
          }  
          System.out.println();  
          bubbleSort(dizi);
-         System.out.println("Sýralý Dizi:");  
+         System.out.println("Sirali Dizi:");  
          for(int i=0; i < dizi.length; i++){  
                  System.out.print(dizi[i] + " ");  
          }  
