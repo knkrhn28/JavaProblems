18213d8950e8678eb5ab5b46115afcf64ea256a9
Okan KARAHAN <oqan_28@hotmail.com>
2017-09-27 14:51:41 +0300

    calculator

---
+++ calculator_Machine.java
@@ -0,0 +1,46 @@
+package CalculatorMachine;
+import java.util.Scanner;
+public class calculator_Machine {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);	
+		double number1,number2,result,select;
+		System.out.println("Ýþlem Seçiniz");
+		System.out.println("1-Toplama\n" + "2-Çýkarma\n"+ "3-Çarpma\n" + "4-Bölme\n");
+		select=input.nextInt();
+		if(select==1) {
+			System.out.println("Number One Please: ");
+			number1=input.nextDouble();
+			System.out.println("Number Two Please: ");
+			number2=input.nextDouble();
+			result=number1+number2;
+			System.out.println("Sum Result: "+result);
+		}
+		else if(select==2) {
+			System.out.println("Number One Please: ");
+			number1=input.nextDouble();
+			System.out.println("Number Two Please: ");
+			number2=input.nextDouble();
+			result=number1-number2;
+			System.out.println("Ex Result: "+result);
+		}
+		else if(select==3) {
+			System.out.println("Number One Please: ");
+			number1=input.nextDouble();
+			System.out.println("Number Two Please: ");
+			number2=input.nextDouble();
+			result=number1*number2;
+			System.out.println("Çarpma Result: "+result);
+		}
+		else if(select==4) {
+			System.out.println("Number One Please: ");
+			number1=input.nextDouble();
+			System.out.println("Number Two Please: ");
+			number2=input.nextDouble();
+			result=(double)number1/(double)number2;
+			System.out.println("Ex Result: "+result);
+		}
+		else {
+			System.out.println("Error Select");
+		}
+	}
+}
